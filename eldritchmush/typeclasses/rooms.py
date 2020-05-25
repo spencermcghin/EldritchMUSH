6eded9813c9778d4513ed577fd0cdc58741e9449
Spencer McGhin <spencer.mcghin@gmail.com>
2020-05-25 11:27:52 -0700

    Added weather room.

--- eldritchmush/typeclasses/rooms.py
+++ eldritchmush/typeclasses/rooms.py
@@ -112,3 +112,57 @@ class ChargenRoom(Room):
     def at_object_creation(self):
         "this is called only at first creation"
         self.cmdset.add(ChargenCmdset, permanent=True)
+
+# Weather room
+
+# These are rainy weather strings
+WEATHER_STRINGS = (
+    "Clouds cover the sky, obscuring might otherwise have been a pleasant day.",
+    "It begins to sprinkle in a soft all-encompassing mist. You would harldy call it rain, though it is certainly wet.",
+    "The rainfall eases a bit and the sky momentarily brightens.",
+    "For a moment it looks like the rain is slowing, then it begins anew with renewed force.",
+    "The rain pummels you with large, heavy drops. You hear the rumble of thunder in the distance.",
+    "The wind is picking up, howling around you, throwing water droplets in your face. It's cold.",
+    "Bright fingers of lightning flash over the sky, moments later followed by a deafening rumble.",
+    "It rains so hard you can hardly see your hand in front of you. You'll soon be drenched to the bone.",
+    "Lightning strikes in several thundering bolts, striking the trees in the forest to your west.",
+    "You hear the distant howl of what sounds like some sort of dog or wolf.",
+    "Large clouds rush across the sky, throwing their load of rain over the world.",
+)
+
+class WeatherRoom(DefaultRoom):
+    """
+    This should probably better be called a rainy room...
+    This sets up an outdoor room typeclass. At irregular intervals,
+    the effects of weather will show in the room. Outdoor rooms should
+    inherit from this.
+    """
+
+    def at_object_creation(self):
+        """
+        Called when object is first created.
+        We set up a ticker to update this room regularly.
+        Note that we could in principle also use a Script to manage
+        the ticking of the room; the TickerHandler works fine for
+        simple things like this though.
+        """
+        super().at_object_creation()
+        # subscribe ourselves to a ticker to repeatedly call the hook
+        # "update_weather" on this object. The interval is randomized
+        # so as to not have all weather rooms update at the same time.
+        self.db.interval = random.randint(50, 70)
+        TICKER_HANDLER.add(
+            interval=self.db.interval, callback=self.update_weather, idstring="tutorial"
+        )
+
+    def update_weather(self, *args, **kwargs):
+        """
+        Called by the tickerhandler at regular intervals. Even so, we
+        only update 20% of the time, picking a random weather message
+        when we do. The tickerhandler requires that this hook accepts
+        any arguments and keyword arguments (hence the *args, **kwargs
+        even though we don't actually use them in this example)
+        """
+        if random.random() < 0.2:
+            # only update 20 % of the time
+            self.msg_contents("|w%s|n" % random.choice(WEATHER_STRINGS))
