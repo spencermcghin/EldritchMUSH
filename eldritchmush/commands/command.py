07578fba33df972ca7f6ff97345c56c2d652b08c
Spencer McGhin <spencer.mcghin@gmail.com>
2020-05-30 21:37:12 -0700

    updating commands

--- eldritchmush/commands/command.py
+++ eldritchmush/commands/command.py
@@ -11,6 +11,7 @@ from django.conf import settings
 # Local imports
 from evennia import Command as BaseCommand
 from evennia import default_cmds, utils, search_object
+from commands import combat
 
 
 _SEARCH_AT_RESULT = utils.object_from_module(settings.SEARCH_AT_RESULT)
