34e3a0de4c0338a0b56cd75d0793eaec281710a6
Spencer McGhin <spencer.mcghin@gmail.com>
2020-05-31 18:35:28 -0700

    updated objects

--- eldritchmush/typeclasses/objects.py
+++ eldritchmush/typeclasses/objects.py
@@ -176,7 +176,7 @@ class ObjTicketBox(DefaultObject):
         # Maintain state of object
         self.db.hasWinner = False
 
-        self.db.desc = "|y\nThis is a large wooden box, carved with filigree and inlaid at odd places with ruddy, fake looking gems. On the top of the box is a small black button. Just beneath this button is a small, tarnished bronze plaque that reads, 'Push Me for a Smile'|n\n
+        self.db.desc = "|y\nThis is a large wooden box, carved with filigree and inlaid at odd places with ruddy, fake looking gems. On the top of the box is a small black button. Just beneath this button is a small, tarnished bronze plaque that reads, 'Push Me for a Smile'|n\n\
         |rOOG - Usage: push button|n"
 
         # Add command set for interacting with box
