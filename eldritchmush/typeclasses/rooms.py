3e7afed1043492a1a194b439e59df4a1bfb7502e
Spencer McGhin <spencer.mcghin@gmail.com>
2020-06-02 09:46:33 -0700

    testing set perception

--- eldritchmush/typeclasses/rooms.py
+++ eldritchmush/typeclasses/rooms.py
@@ -111,9 +111,9 @@ class Room(DefaultRoom):
                 at the given perceptionkey.
         """
         if self.db.perception_details:
-            self.db.perception_details[perceptionkey.lower()].append((level, description))
+            self.db.perception_details[perceptionkey.name.lower()].append((level, description))
         else:
-            self.db.perception_details = {perceptionkey.lower(): [(level, description)]}
+            self.db.perception_details = {perceptionkey.name.lower(): [(level, description)]}
 
     def set_tracking(self, trackingkey, level, description):
         """
