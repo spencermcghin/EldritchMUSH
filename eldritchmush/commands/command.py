1cb105e440cbd6debd784a52c6535466593bb9a5
Spencer McGhin <spencer.mcghin@gmail.com>
2020-06-02 19:01:17 -0700

    Set perc is finally working, or did the commit before this.

--- eldritchmush/commands/command.py
+++ eldritchmush/commands/command.py
@@ -1092,14 +1092,11 @@ class CmdPerception(default_cmds.MuxCommand):
             )
             if looking_at_obj:
                 self.obj = looking_at_obj[0]
-                self.caller.msg(f"You are looking at {self.obj}")
-            #     # Check to see if there are search results
-            #     else:
+                # self.caller.msg(f"You are looking at {self.obj}")
                 # Set the perception object in the database
                 self.obj.set_perception(self.obj.name, level, self.rhs)
-            #
-            #         # Message to admin for confirmation.
-            #         self.caller.msg(f"Perception set on {object}\nLevel: {level}\nDescription: {self.rhs}")
+                # Message to admin for confirmation.
+                self.caller.msg(f"Perception set on {self.obj.name}\nLevel: {level}\nDescription: {self.rhs}")
             else:
                 self.caller.msg("Search didn't return anything.")
 
