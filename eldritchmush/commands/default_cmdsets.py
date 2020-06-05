6d5422611e9aec5a8a108f55d861705cc67ff1e6
Spencer McGhin <spencer.mcghin@gmail.com>
2020-06-04 20:21:20 -0700

    Added setshieldlevel to command set for character

--- eldritchmush/commands/default_cmdsets.py
+++ eldritchmush/commands/default_cmdsets.py
@@ -82,6 +82,7 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         self.add(combat.CmdKill())
         self.add(combat.CmdBattlefieldCommander())
         self.add(combat.CmdRally())
+        self.add(command.SetShieldLevel())
 
 
 class ArtessaCmdSet(RoomCmdSet):
