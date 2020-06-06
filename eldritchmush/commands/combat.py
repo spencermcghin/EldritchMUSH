ba7131f80182c4787b65c0369b339a6b88d335b9
Spencer McGhin <spencer.mcghin@gmail.com>
2020-06-06 15:59:07 -0700

    updated typo in shoot

--- eldritchmush/commands/combat.py
+++ eldritchmush/commands/combat.py
@@ -403,8 +403,8 @@ class CmdShoot(Command):
 
             # Compare caller attack_result to target av.
             # If attack_result > target av -> hit, else miss
-            if attack_result > target_av:
-                self.caller.location.msg_contents(f"|b{self.caller.key} lets loose an arrow ({attack_result}) straight for {target.key}'s {shot_location} and hits|n (|r{target_av}|n), |bdealing|n |y{damage}|n |bdamage!|n")
+            if attack_result > target.db.av:
+                self.caller.location.msg_contents(f"|b{self.caller.key} lets loose an arrow ({attack_result}) straight for {target.key}'s {shot_location} and hits|n (|r{target.db.av}|n), |bdealing|n |y{damage}|n |bdamage!|n")
                 # subtract damage from corresponding target body
                 target.db.body -= damage
                 # Send a message to the target, letting them know their body values
