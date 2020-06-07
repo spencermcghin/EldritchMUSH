c75f77648c6797885b811c958306fe3e832f3def
Spencer McGhin <spencer.mcghin@gmail.com>
2020-06-06 21:48:15 -0700

    updated disarm

--- eldritchmush/commands/combat.py
+++ eldritchmush/commands/combat.py
@@ -586,15 +586,14 @@ class CmdDisarm(Command):
                     attack_result = (die_result + weapon_level) - dmg_penalty - weakness
 
                     # Return attack result message
-                    if self.caller.db.disarm > 0:
-                        if attack_result >= target.db.av:
-                            self.caller.location.msg_contents(f"|025{self.caller.key} counters the next attack by disarming {target.key} for the round.|n")
-                            self.caller.db.body += 1
-                            self.caller.msg(f"|540Disarm adds one body back to you total.\nYour new total body value is {self.caller.db.body}|n")
-                        elif attack_result < target.db.av:
-                            self.caller.location.msg_contents(f"|025{self.caller.key} attempts|n (|400{attack_result}|n|025 to disarm {target.key}|n (|020{target.db.av}|n|025, but fumbles their attack.|n")
-                    else:
-                        self.caller.msg("|400You have 0 disarms remaining.")
+                    if attack_result >= target.db.av:
+                        self.caller.location.msg_contents(f"|025{self.caller.key} counters the next attack by disarming {target.key} for the round.|n")
+                        self.caller.db.body += 1
+                        self.caller.msg(f"|540Disarm adds one body back to you total.\nYour new total body value is {self.caller.db.body}|n")
+                    elif attack_result < target.db.av:
+                        self.caller.location.msg_contents(f"|025{self.caller.key} attempts|n (|400{attack_result}|n|025 to disarm {target.key}|n (|020{target.db.av}|n|025, but fumbles their attack.|n")
+                else:
+                    self.caller.msg("|400You have 0 disarms remaining.")
 
 
 class CmdStun(Command):
@@ -668,6 +667,7 @@ class CmdStun(Command):
                     attack_result = (die_result + weapon_level) - dmg_penalty - weakness
 
                     # Return attack result message
+
                     self.caller.location.msg_contents(f"|025{self.caller.key} goes to stun {self.target} such that they're unable to attack for a moment.|n\n|540{self.target.key} may not attack next round if {attack_result} is a successful hit.|n")
                 else:
                     self.caller.msg("|400You have 0 stuns remaining.|n")
