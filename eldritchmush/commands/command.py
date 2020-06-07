d6fc76779efb9fae86ea78e9f0ecf09d9a652058
Spencer McGhin <spencer.mcghin@gmail.com>
2020-06-07 11:32:08 -0700

    Testing box objects

--- eldritchmush/commands/command.py
+++ eldritchmush/commands/command.py
@@ -1209,11 +1209,11 @@ class CmdPushButton(Command):
                 # Should indicate that character picks it up.
                 self.caller.msg(get_ticket_emote)
                 self.caller.location.msg_contents(button_emote)
-                self.execute_cmd(skull_command)
+                self.obj.execute_cmd(skull_command)
             else:
                 # Drop a ticket object with a skull description
                 self.caller.location.msg_contents(button_emote)
-                self.execute_cmd(jester_command)
+                self.obj.execute_cmd(jester_command)
         else:
             self.caller.msg("|yUsage: push button|n")
 
