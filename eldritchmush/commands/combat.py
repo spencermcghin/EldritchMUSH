200075ce525e0f4324d5827000087bd633712966
Spencer McGhin <spencer.mcghin@gmail.com>
2020-06-06 20:44:09 -0700

    updated message when bleeding/dying

--- eldritchmush/commands/combat.py
+++ eldritchmush/commands/combat.py
@@ -351,8 +351,10 @@ class CmdShoot(Command):
                     target.msg(f"|540Your new body value is {target.db.body}|n")
                     if -3 <= target.db.body <= 0:
                         target.msg("|540You are bleeding profusely from many wounds and can no longer use any active martial skills.\nYou may only use the limbs that have not been injured.|n")
+                        target.msg.location.msg_contents(f"|025{target.key} is bleeding profusely from many wounds and will soon lose consciousness.|n")
                     elif target.db.body <= -4:
                         target.msg("|400You are now unconscious and can no longer move of your own volition.|n")
+                        target.msg.location.msg_contents(f"|025{target.key} does not seem to be moving.|n")
             else:
                 # No target armor so subtract from their body total and hit a limb. Add logic from handler above. Leave in body handler in combat handler.
                 self.caller.location.msg_contents(f"|025{self.caller.key} lets loose an arrow |n(|400{attack_result}|n)|025 at {target.key}|n(|020{target.db.av}|n)|025, but it misses.|n")
@@ -449,9 +451,11 @@ class CmdCleave(Command):
                         # Send a message to the target, letting them know their body values
                         target.msg(f"|540Your new body value is {target.db.body}|n")
                         if -3 <= target.db.body <= 0:
+                            target.msg.location.msg_contents(f"|025{target.key} is bleeding profusely from many wounds and will soon lose consciousness.|n")
                             target.msg("|540You are bleeding profusely from many wounds and can no longer use any active martial skills.\nYou may only use the limbs that have not been injured.|n")
                         elif target.db.body <= -4:
                             target.msg("|400You are now unconscious and can no longer move of your own volition.|n")
+                            target.msg.location.msg_contents(f"|025{target.key} does not seem to be moving.|n")
                 else:
                     # No target armor so subtract from their body total and hit a limb. Add logic from handler above. Leave in body handler in combat handler.
                     self.caller.location.msg_contents(f"|025{self.caller.key} strikes with great ferocity|n (|400{attack_result}|n)|025 at {target.key}|n(|020{target.db.av}|n)|025, but it misses.|n")
