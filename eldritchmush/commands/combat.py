23f8e30932f9e31a50b56a0c813478a90ba8d2aa
Spencer McGhin <spencer.mcghin@gmail.com>
2020-06-06 22:05:58 -0700

    updated cleave to throw error if not two handed

--- eldritchmush/commands/combat.py
+++ eldritchmush/commands/combat.py
@@ -422,49 +422,52 @@ class CmdCleave(Command):
             self.caller.msg("|540Before you can attack, you must first equip a weapon using the command setmelee 1.|n")
         else:
             # Check to make sure caller has cleaves remaining
-            if cleavesRemaining > 0:
+            if self.caller.db.twohanded:
 
-                # Return die roll based on level in master of arms or wylding hand.
-                if wylding_hand:
-                    die_result = h.wyldingHand(wylding_hand)
-                else:
-                    die_result = h.masterOfArms(master_of_arms)
+                if cleavesRemaining > 0:
+                    # Return die roll based on level in master of arms or wylding hand.
+                    if wylding_hand:
+                        die_result = h.wyldingHand(wylding_hand)
+                    else:
+                        die_result = h.masterOfArms(master_of_arms)
 
-                # Decrement amount of cleaves from amount in database
-                self.caller.db.cleave -= 1
+                    # Decrement amount of cleaves from amount in database
+                    self.caller.db.cleave -= 1
 
-                # Get final attack result and damage
-                dmg_penalty = h.bodyChecker(self.caller.db.body)
-                attack_result = (die_result + weapon_level) - dmg_penalty
-                shot_location = h.shotFinder(target.db.targetArray)
+                    # Get final attack result and damage
+                    dmg_penalty = h.bodyChecker(self.caller.db.body)
+                    attack_result = (die_result + weapon_level) - dmg_penalty
+                    shot_location = h.shotFinder(target.db.targetArray)
 
-                if attack_result >= target.db.av:
-                    self.caller.location.msg_contents(f"|025{self.caller.key} strikes|n (|020{attack_result}|n) |025with great ferocity and cleaves {target.key}'s {shot_location}|n (|400{target.db.av}|n)|025! dealing|n |5402|n |025damage|n.")
-                    if shot_location == "torso":
-                        if target.db.body > 0:
-                            target.db.body = 0
-                            self.caller.location.msg_contents(f"|025{target.key} has been fatally wounded and is now bleeding to death. They will soon be unconscious.|n")
+                    if attack_result >= target.db.av:
+                        self.caller.location.msg_contents(f"|025{self.caller.key} strikes|n (|020{attack_result}|n) |025with great ferocity and cleaves {target.key}'s {shot_location}|n (|400{target.db.av}|n)|025! dealing|n |5402|n |025damage|n.")
+                        if shot_location == "torso":
+                            if target.db.body > 0:
+                                target.db.body = 0
+                                self.caller.location.msg_contents(f"|025{target.key} has been fatally wounded and is now bleeding to death. They will soon be unconscious.|n")
+                            else:
+                                target.db.body -= 2
+                                target.msg(f"|540Your new body value is {target.db.body}|n")
                         else:
                             target.db.body -= 2
+                            target.msg(f"|400You {shot_location} is now injured and have taken |n|5402l|n|400 points of damage.|n")
+                            # Send a message to the target, letting them know their body values
                             target.msg(f"|540Your new body value is {target.db.body}|n")
+                            if -3 <= target.db.body <= 0:
+                                target.location.msg_contents(f"|025{target.key} is bleeding profusely from many wounds and will soon lose consciousness.|n")
+                                target.msg("|540You are bleeding profusely from many wounds and can no longer use any active martial skills.\nYou may only use the limbs that have not been injured.|n")
+                            elif target.db.body <= -4:
+                                target.msg("|400You are now unconscious and can no longer move of your own volition.|n")
+                                target.location.msg_contents(f"|025{target.key} does not seem to be moving.|n")
                     else:
-                        target.db.body -= 2
-                        target.msg(f"|400You {shot_location} is now injured and have taken |n|5402l|n|400 points of damage.|n")
-                        # Send a message to the target, letting them know their body values
-                        target.msg(f"|540Your new body value is {target.db.body}|n")
-                        if -3 <= target.db.body <= 0:
-                            target.location.msg_contents(f"|025{target.key} is bleeding profusely from many wounds and will soon lose consciousness.|n")
-                            target.msg("|540You are bleeding profusely from many wounds and can no longer use any active martial skills.\nYou may only use the limbs that have not been injured.|n")
-                        elif target.db.body <= -4:
-                            target.msg("|400You are now unconscious and can no longer move of your own volition.|n")
-                            target.location.msg_contents(f"|025{target.key} does not seem to be moving.|n")
+                        # No target armor so subtract from their body total and hit a limb. Add logic from handler above. Leave in body handler in combat handler.
+                        self.caller.location.msg_contents(f"|025{self.caller.key} strikes with great ferocity|n (|400{attack_result}|n)|025 at {target.key}|n(|020{target.db.av}|n)|025, but it misses.|n")
+
                 else:
-                    # No target armor so subtract from their body total and hit a limb. Add logic from handler above. Leave in body handler in combat handler.
-                    self.caller.location.msg_contents(f"|025{self.caller.key} strikes with great ferocity|n (|400{attack_result}|n)|025 at {target.key}|n(|020{target.db.av}|n)|025, but it misses.|n")
+                    self.caller.msg("|400You have 0 cleaves remaining.")
 
             else:
-                self.caller.msg("|400You have 0 cleaves remaining.")
-
+                self.caller.msg("|400You cannot use this attack without first equipping a two handed weapon using the command settwohanded 1.|n")
 
 class CmdResist(Command):
     """
