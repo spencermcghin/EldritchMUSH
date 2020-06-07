1db07716058b25cf54b124a99960858b392c1b8a
Spencer McGhin <spencer.mcghin@gmail.com>
2020-06-06 21:03:52 -0700

    updating colors and cleave

--- eldritchmush/typeclasses/characters.py
+++ eldritchmush/typeclasses/characters.py
@@ -90,10 +90,10 @@ class Character(DefaultCharacter):
         isDying = True if (-6 <= self.db.body <= -4) else False
         # target = text.split("\n")
         if isBleeding:
-            return text + f"\n|r{self.key} is bleeding profusely from mutliple wounds. They may need a healer.|n"
+            return text + f"\n|400{self.key} is bleeding profusely from mutliple wounds. They may need a healer.|n"
 
         elif isDying:
-            return text + f"\n|R{self.key} is now unconscious. They will soon surely be dead.|n"
+            return text + f"\n|400{self.key} is now unconscious. They will soon surely be dead.|n"
 
         else:
             return text
