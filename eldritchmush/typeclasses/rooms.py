3f454bf2f1212439978da5edc521a234fc1da7bb
Spencer McGhin <spencer.mcghin@gmail.com>
2020-06-07 20:08:47 -0700

    Updated ophidia ticker

--- eldritchmush/typeclasses/rooms.py
+++ eldritchmush/typeclasses/rooms.py
@@ -505,8 +505,8 @@ class OphidiaRoom(Room):
             current_index = 0
             # Retrieve a new market broadcast that has not been played yet.
             phrase = OPHIDIA_STRINGS[current_index]
-            self.msg_contents("%s" % phrase)
-            if current_index == len(OPHIDIA_STRINGS) - 1:
+            if current_index > len(OPHIDIA_STRINGS) - 1:
                 TICKER_HANDLER.remove(2, self.update_show, idstring="ophidia_start_show_ticker")
             else:
+                self.msg_contents("%s" % phrase)
                 current_index += 1
