226d361c4b9927aa75b6c188a75c955e0df86290
Spencer McGhin <spencer.mcghin@gmail.com>
2020-06-08 19:20:36 -0700

    typo

--- eldritchmush/commands/default_cmdsets.py
+++ eldritchmush/commands/default_cmdsets.py
@@ -82,7 +82,7 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         self.add(command.CmdMedicine())
         self.add(command.SetWeakness())
         self.add(combat.CmdBattlefieldCommander())
-        self.add(combat.CmdBattlefieldMedicine())
+        self.add(command.CmdBattlefieldMedicine())
         self.add(combat.CmdRally())
         self.add(command.SetShieldValue())
         self.add(command.CharSheet())
