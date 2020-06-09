327121ef5b095cdd0ac3f658f876aad831835136
Spencer McGhin <spencer.mcghin@gmail.com>
2020-06-08 18:37:53 -0700

    Upated medicine and formatting

--- eldritchmush/commands/command.py
+++ eldritchmush/commands/command.py
@@ -1256,9 +1256,35 @@ class CmdMedicine(Command):
             if (- 3 <= target_body <= 0):
                 # Return message to area and caller
                 if target == self.caller:
-                    self.caller.location.msg_contents(f"|015{self.caller} pulls bandages and ointments from their bag, and starts to mend their wounds.|n\n|540{self.caller} heals |n|030{medicine}|n |540body points per round as long as their work remains uninterrupted for the round.|n")
+                    self.caller.location.msg_contents(f"|015{self.caller} pulls bandages and ointments from their bag, and starts to mend their wounds.|n\n|540{self.caller} heals |n|020{medicine}|n |540body points per round as long as their work remains uninterrupted.|n")
+                    # Check to see if caller would go over 1 body with application of skill.
+                    if (self.caller.db.body + medicine) > 1:
+                        # If so set body to 1
+                        self.caller.db.body = 1
+                        self.caller.msg(f"|540Your new body value is:|n {self.caller.db.body}|n")
+                    else:
+                        # If not over 1, add points to total
+                        self.caller.db.body += medicine
+                        self.caller.msg(f"|540Your new body value is:|n {self.caller.db.body}|n")
+                    # Check to see if weakness set. If not, set it.
+                    if not weakness:
+                        self.caller.db.weakness = 1
+
                 elif target != self.caller:
-                    self.caller.location.msg_contents(f"|015{self.caller.key} comes to {target.key}'s rescue, healing {target.key}.|n\n|540{self.caller.key} heals {target.key} for|n |030{medicine}|n |540body points per round as long as their work remains uninterrupted for the round.|n")
+                    self.caller.location.msg_contents(f"|015{self.caller.key} comes to {target.key}'s rescue, healing {target.key}.|n\n|540{self.caller.key} heals {target.key} for|n |020{medicine}|n |540body points per round as long as their work remains uninterrupted.|n")
+                    if (target.db.body + medicine) > 1:
+                        # If so set body to 1
+                        target.db.body = 1
+                        target.msg(f"|540Your new body value is:|n {target.db.body}|n")
+                        # Check to see if weakness set. If not, set it.
+                    else:
+                        # If not over 1, add points to total
+                        target.db.body += medicine
+                        target.msg(f"|540Your new body value is:|n {target.db.body}|n")
+                    # Check to see if weakness set. If not, set it.
+                    if not weakness:
+                        target.db.weakness = 1
+
 
             # Apply stabilize to other target
             elif (-6 <= target_body <= -4):
@@ -1310,7 +1336,7 @@ class CmdStabilize(Command):
             if (- 3 <= target_body <= 0) and medicine:
                 # Return message to area and caller
                 if target == self.caller:
-                    self.caller.location.msg_contents(f"|015{self.caller} pulls bandages and ointments from their bag, and starts to mend their wounds.\n|540{self.caller} heals|n |030{medicine}|n |540body points per round as long as their work remains uninterrupted.|n")
+                    self.caller.location.msg_contents(f"|015{self.caller} pulls bandages and ointments from their bag, and starts to mend their wounds.\n|540{self.caller} heals|n |020{medicine}|n |540body points per round as long as their work remains uninterrupted.|n")
 
                     # Check to see if caller would go over 1 body with application of skill.
                     if (self.caller.db.body + medicine) > 1:
@@ -1327,7 +1353,7 @@ class CmdStabilize(Command):
 
                 # If target is someone else, do checks and apply healing.
                 elif target != self.caller and medicine:
-                    target.location.msg_contents(f"|015{self.caller.key} comes to {target.key}'s rescue, healing {target.key} for|n |030{medicine}|n |015body points.|n")
+                    target.location.msg_contents(f"|015{self.caller.key} comes to {target.key}'s rescue, healing {target.key} for|n |020{medicine}|n |015body points.|n")
                     if (target.db.body + medicine) > 1:
                         # If so set body to 1
                         target.db.body = 1
@@ -1360,7 +1386,7 @@ class CmdStabilize(Command):
                         self.caller.db.weakness = 1
 
                 elif target != self.caller:
-                    target.location.msg_contents(f"|015{self.caller.key} comes to {target.key}'s rescue, healing {target.key} for|n |030{stabilize}|n |015body points.|n")
+                    target.location.msg_contents(f"|015{self.caller.key} comes to {target.key}'s rescue, healing {target.key} for|n |020{stabilize}|n |015body points.|n")
                     if (target.db.body + stabilize) > 1:
                         # If so set body to 1
                         target.db.body = 1
