780035abcc06f444adfca045bcdab7554187541b
Spencer McGhin <spencer.mcghin@gmail.com>
2020-07-09 21:18:32 -0700

    Added functions to combat loop rules set.

--- eldritchmush/world/combat_loop.py
+++ eldritchmush/world/combat_loop.py
@@ -17,7 +17,7 @@ list, the rules system checks to make sure the command is acceptable.
 4. If the command belongs to the list of combat commands, the code checks to
 see if the character is currently in the room's combat loop.
 
-5. If the command does not belong to the combat loop, the character is prompted
+5. If the character does not belong to the combat loop, the character is prompted
 to enter a command from the list and/or to disengage from the fight if they do not
 wish to take part.
 
@@ -29,3 +29,53 @@ combat loop.
 wait until it is their turn (combat_turn) before they are able to enter a command.
 
 """
+class CombatLoop:
+
+    # Acceptable COMBAT_COMMANDS
+    COMBAT_COMMANDS = ["strike",
+                       "hit",
+                       "slash",
+                       "bash",
+                       "punch",
+                       "shoot",
+                       "cleave",
+                       "disarm",
+                       "stun",
+                       "stagger",
+                       "sunder"]
+
+
+    def checkCommand(self, command: str) -> bool:
+        if command in COMBAT_COMMANDS:
+            return True
+        else:
+            return False
+
+
+    def isTurn(self, character):
+        if character.db.combat_turn:
+            return True
+        else:
+            return False
+
+
+    def checkLoop(self, character):
+        # Get room where character is calling the combat command.
+        current_room = character.location.dbref
+        combat_loop = current_room.db.combat_loop
+
+        # Check to see if caller is part of rooms combat loop
+        if character in combat_loop:
+            return True
+        else:
+            return False
+
+
+    def addCharacter(self, character):
+        # TODO: get character return type
+        # Get room where character is calling the combat command.
+        current_room = character.location.dbref
+        combat_loop = current_room.db.combat_loop
+
+        # Add character to combat loop
+        combat_loop.append(character)
