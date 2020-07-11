7748a77d18d2fceae44c660fc8aae5ddec6a939a
Spencer McGhin <spencer.mcghin@gmail.com>
2020-07-11 14:08:43 -0700

    Updating combat loop to include logic in parse before executing logic.

--- eldritchmush/world/combat_loop.py
+++ eldritchmush/world/combat_loop.py
@@ -1,3 +1,4 @@
+
 """
 Combat Loop
 
@@ -8,74 +9,129 @@ the mush space.
 database. A combat loop attribute has also been added to the Room class.
 
 2. There can only be one combat_loop in a room at a time. A character may add and
-remove themselves from the rooms combat loop as they are able, using the
-corresponding combat commands.
+remove themselves from the room's combat loop as they are able, using the
+corresponding combat commands. The player may then only exit the combat loop when
+brought to 0 body or by using the corresponding exit command (Disengage in this case.)
 
-3. Once a character uses one of the commands as contained in the COMBAT_COMMANDS
-list, the rules system checks to make sure the command is acceptable.
+3. Combat loops are referenced each time one of the combat commands is called.
 
-4. If the command belongs to the list of combat commands, the code checks to
-see if the character is currently in the room's combat loop.
+4. Logic checks if combat_loop is empty before adding a new character.
 
-5. If the character does not belong to the combat loop, the character is prompted
-to enter a command from the list and/or to disengage from the fight if they do not
-wish to take part.
+5. If loop is not empty, append to end of loop and set character's combat turn to 0.
+Their combat command set should now be disabled. Another option: If character tries to
+enter combat command, logic checks if it's their turn. The character will be prompted
+that they've been added to the loop, then be prompted that they'll need to wait
+until it is their turn.
 
-6. If the character is not listed in the room's combat loop, they are added to
-the end of the round, or interpreted another way, to the end of the room's
-combat loop.
+6. Character's turn number is checked by logic. Their turn number in the round
+will then be shown to them.
 
-7. If the character is currently engaged in combat, they will be prompted to
-wait until it is their turn (combat_turn) before they are able to enter a command.
+7. If loop is empty, append caller to loop.
+
+8. Add target to combat loop and set target's combat_turn to 0.
+
+9. At end of combat command resolution, switch combat_turn on caller to 0.
+Ensure attacker can't do combat commands via handling.
+
+9. Check for number of elements in combat loop. If greater than 1, go to next
+element (character/npc).
+
+9. Switch the next character/npc in the loop's combat_turn to 1. Prompt them
+to enter command.
+
+10. If number of elements left in loop <= 1, remove remaining elements in loop.
+Combat loop is empty and now ready for a new combat.
+
+Notes:
+Handle removal of character if body is 0 or less - check before setting combat_turn
+to 1 if character is at 0 or less. If so, skip to next element in list. If > 0, proceed
+as normal.
+If bleeding, you can still use disengage, but only that command.
+If dying, you can do nothing and be the target of a drag command
+Build command - Drag. Your turn is skipped.
+Need to fix it so you can see NPCs msgs
+
+Hook ideas
+
 
 """
+
 class CombatLoop:
 
-    # Acceptable COMBAT_COMMANDS
-    COMBAT_COMMANDS = ["strike",
-                       "hit",
-                       "slash",
-                       "bash",
-                       "punch",
-                       "shoot",
-                       "cleave",
-                       "disarm",
-                       "stun",
-                       "stagger",
-                       "sunder"]
+    def __init__(self, caller, target, combatCommand):
+        self.caller = caller
+        self.target = target
+        self.combatCommand = combatCommand
+        self.current_room = self.character.location.dbref
+        self.combat_loop = self.current_room.db.combat_loop
 
 
-    def checkCommand(self, command: str) -> bool:
-        if command in COMBAT_COMMANDS:
-            return True
-        else:
-            return False
-
-
-    def isTurn(self, character):
-        if character.db.combat_turn:
-            return True
-        else:
-            return False
-
-
-    def checkLoop(self, character):
-        # Get room where character is calling the combat command.
-        current_room = character.location.dbref
-        combat_loop = current_room.db.combat_loop
-
+    def inLoop(self):
         # Check to see if caller is part of rooms combat loop
-        if character in combat_loop:
+        if self.caller in self.combat_loop:
             return True
         else:
             return False
 
 
-    def addCharacter(self, character):
-        # TODO: get character return type
-        # Get room where character is calling the combat command.
-        current_room = character.location.dbref
-        combat_loop = current_room.db.combat_loop
+    def isTurn(self):
+        if self.caller.db.combat_turn:
+            return True
+        else:
+            return False
 
+
+    def combatTurnOn(self, combatant):
+        combatant.db.combat_turn = 0
+
+
+    def combatTurnOff(self, combatant):
+        combatant.db.combat_turn = 1
+
+
+    def addToLoop(self, combatant):
         # Add character to combat loop
-        combat_loop.append(character)
+        self.combat_loop.append(combatant)
+
+
+    def getCombatTurn(self, combatant):
+        # Set character turn to index of character in combat loop, plus one
+        return self.combat_loop.index(combatant) + 1
+
+
+    def getLoopLength(self):
+        return len(self.combat_loop)
+
+
+    # Main logic
+    def resolveCommand(self):
+        loopLength = self.getLoopLength()
+
+        # If character not in loop and loop is empty
+        if not self.caller.inLoop and loopLength == 0:
+
+            # Add character to loop
+            self.addToLoop(self.caller)
+            # Send message to attacker and resolve command
+            self.caller.msg(f"You have been added to the combat loop for the {self.current_room}")
+
+            # Add target of attack to loop
+            self.addToLoop(self.target)
+            # Send message to target and resolve command
+            targetTurn = self.getCombatTurn(self.target)
+            self.target.msg(f"You have been added to the combat loop for the {self.current_room}.\nYou are currently number {targetTurn} in the round order.")
+            # Disable their ability to use combat commands
+            self.combatTurnOff(self.target)
+
+            # Caller executes command
+            self.caller.execute_cmd(self.combatCommand)
+
+            # Clean up
+            # Set caller's combat_turn to 0. Can no longer use combat commands.
+            self.combatTurnOff(self.caller)
+
+
+        elif not self.caller.inLoop and loopLength > 0:
+
+            # Append to end of loop
+            self.combat_loop.append(self.caller)
