560f5b36548df7b202f446cdbc54e9a0bbe1ea93
Spencer McGhin <spencer.mcghin@gmail.com>
2020-07-11 21:31:24 -0700

    update strike

--- eldritchmush/commands/combat_commands/strike_command/strike.py
+++ eldritchmush/commands/combat_commands/strike_command/strike.py
@@ -56,7 +56,7 @@ class CmdStrike(Command):
     def func(self):
 
         # Run logic for strike command
-        if self.caller.isTurn():
+        if loop.isTurn(self.caller):
             strike.runLogic()
         else:
             self.caller.msg("You need to wait until it is your turn before you are able to act.")
