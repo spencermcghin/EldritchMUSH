f5a079d528c3e6702e7c3c2f1d95a090a0fb6dd4
Spencer McGhin <spencer.mcghin@gmail.com>
2020-07-17 22:11:32 -0700

    Testing disengage logic

--- eldritchmush/commands/combat_commands/disengage.py
+++ eldritchmush/commands/combat_commands/disengage.py
@@ -16,15 +16,17 @@ class CmdDisengage(Command):
     aliases = ["disengage", "escape", "flee"]
     help_category = "combat"
 
+    combat_loop = self.caller.location.db.combat_loop
+    caller_index = combat_loop.index(self.caller.key)
+
     def func(self):
         "Implements the command"
-        combat_loop = self.caller.location.db.combat_loop
-        caller_index = combat_loop.index(self.caller.key)
 
         # Check if it is player's combat_turn
         if self.caller.db.combat_turn:
             # Check to see if caller is in combat loop:
             if self.caller.key in combat_loop:
+
                 # Check if combatant is at last index before disengaging and then passing turn
                 # Loop should never be less than 1 given cleanup step.
                 if len(combat_loop) > 1 and len(combat_loop) == caller_index + 1:
@@ -38,6 +40,10 @@ class CmdDisengage(Command):
                     self.caller.db.combat_turn = 1
                     self.msg("You have disengaged from combat.")
                     self.caller.location.msg_contents(f"{self.caller.key} breaks away from combat.")
+
+                    # Run disengage_cleanup
+                    self.disengage_cleanup()
+
                 else:
                     # Go to next player instead and set combat_turn to 1
                     next_combatant_str = caller_index + 1
@@ -48,9 +54,24 @@ class CmdDisengage(Command):
                     # Set combat_turn back to 1
                     self.caller.db.combat_turn = 1
                     first_combatant_key.msg("You have disengaged from combat.")
-                    self.caller.location.msg_contents(f"{first_combatant_key} breaks away from combat.")                    next
+                    self.caller.location.msg_contents(f"{first_combatant_key} breaks away from combat.")
+
+                    # Run disengage_cleanup
+                    self.disengage_cleanup()
 
             else:
                 self.msg(f"You are not part of the combat loop for {self.caller.location}.")
         else:
             self.caller.msg("You need to wait until it is your turn before you are able to act.")
+
+
+    def disengage_cleanup(self):
+        if len(combat_loop) == 1:
+            # Remove remaining combatants from combat loop and prompt
+            remaining_str = combat_loop.pop()
+            remaining_key = self.caller.search(remaining_str)
+            remaining_key.msg(f"Combat is over. You have been removed from the combat loop for {self.caller.location}")
+            remaining_key.db.combat_turn = 1
+
+        else:
+            return
