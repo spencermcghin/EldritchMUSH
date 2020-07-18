b12b1958eee8866994397bf9ac6b7d227a7dc71b
Spencer McGhin <spencer.mcghin@gmail.com>
2020-07-17 21:58:15 -0700

    updating disengage for multiple states

--- eldritchmush/commands/combat_commands/disengage.py
+++ eldritchmush/commands/combat_commands/disengage.py
@@ -19,41 +19,38 @@ class CmdDisengage(Command):
     def func(self):
         "Implements the command"
         combat_loop = self.caller.location.db.combat_loop
+        caller_index = combat_loop.index(self.caller.key)
 
         # Check if it is player's combat_turn
         if self.caller.db.combat_turn:
-
-            # Check to see if they are in the combat loop for their current room
+            # Check to see if caller is in combat loop:
             if self.caller.key in combat_loop:
-                # Get and hold next combatant value to move to next turn
-                # TODO: Handle for player 
-                next_combatant = combat_loop.index(self.caller.key) + 1
-                next_comb_key = self.caller.search(combat_loop[next_combatant])
-                # Remove from combat loop
-                combat_loop.remove(self.caller.key)
-                # Set combat_turn back to 1
-                self.caller.db.combat_turn = 1
-                # To disengage
-                self.msg("|540You disengage from the attack.|n")
-                self.caller.location.msg_contents(f"|015{self.caller} backs away and then disengages from the fight.|n")
-
-                # Check to see if just one person left in loop. Remove if so and
-                # set turn to 1
-                if len(combat_loop) == 1:
-                    combatant = combat_loop[0]
-                    combat_loop.remove(combatant)
-                    comb_key = self.caller.search(combatant)
-                    comb_key.db.combat_turn = 1
-
+                # Check if combatant is at last index before disengaging and then passing turn
+                # Loop should never be less than 1 given cleanup step.
+                if len(combat_loop) > 1 and len(combat_loop) == caller_index + 1:
+                    # Get and hold next combatant value to move to next turn
+                    first_combatant_str = combat_loop[0]
+                    first_combatant_key = self.caller.search(first_combatant_str)
+                    first_combatant_key.db.combat_turn = 1
+                    # Remove caller from combat loop
+                    combat_loop.remove(self.caller.key)
+                    # Set combat_turn back to 1
+                    self.caller.db.combat_turn = 1
+                    self.msg("You have disengaged from combat.")
+                    self.caller.location.msg_contents(f"{self.caller.key} breaks away from combat.")
                 else:
-                    # Go to the next combatant's turn
-                    next_combatant.msg("Foo")
-
+                    # Go to next player instead and set combat_turn to 1
+                    next_combatant_str = caller_index + 1
+                    next_combatant_key = self.caller.search(next_combatant_str)
+                    next_combatant_key.db.combat_turn = 1
+                    # Remove caller from combat loop
+                    combat_loop.remove(self.caller.key)
+                    # Set combat_turn back to 1
+                    self.caller.db.combat_turn = 1
+                    first_combatant_key.msg("You have disengaged from combat.")
+                    self.caller.location.msg_contents(f"{first_combatant_key} breaks away from combat.")                    next
 
             else:
-                self.msg(f"{self.caller.key}, you are not part of this location's combat loop.")
-
+                self.msg(f"You are not part of the combat loop for {self.caller.location}.")
         else:
-
-            self.msg("You need to wait until it is your turn before you are able to act.")
-            return
+            self.caller.msg("You need to wait until it is your turn before you are able to act.")
