f01f11e90650a63d8b2cf435d404603a52b2dc0d
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-01 14:39:15 -0700

    updated bm

--- eldritchmush/commands/combat_commands/battlefield_medicine.py
+++ eldritchmush/commands/combat_commands/battlefield_medicine.py
@@ -2,8 +2,6 @@
 from evennia import Command
 from world.combat_loop import CombatLoop
 from commands.combat import Helper
-from commands.combat_commands.battlefield_med_handler import HealingHandler
-
 
 class CmdBattlefieldMedicine(Command):
     key = "medic"
@@ -36,53 +34,53 @@ class CmdBattlefieldMedicine(Command):
         loop.resolveCommand()
 
         if caller.db.combat_turn:
-            if h.canFight(self.caller):
+            if h.canFight(caller):
                 if battlefieldmedicine and target_body is not None:
                     # Use parsed args in combat loop. Handles turn order in combat.
                     # Resolve medic command
-                    if 1 <= self.target.db.body <= 3:
+                    if 1 <= target.db.body <= 3:
                         # Return message to area and caller
-                        if self.target == self.caller:
+                        if target == caller:
                             # Check to see if caller would go over 1 body with application of skill.
-                            if (self.caller.db.body + 1) > 3:
+                            if (caller.db.body + 1) > 3:
                                 # If so set body to 1
-                                self.caller.db.body = 3
-                                self.caller.msg(f"|230{self.target.key} doesn't require the application of your chiurgical skills. They seem to be healthy enough.|n")
+                                caller.db.body = 3
+                                caller.msg(f"|230{target.key} doesn't require the application of your chiurgical skills. They seem to be healthy enough.|n")
                                 return
                             else:
                                 # If not over 1, add points to total
-                                self.caller.location.msg_contents(f"|230{self.caller} pulls bandages and ointments from their bag, and starts to mend their wounds.|n\n|540{self.caller} heals |n|0201|n |540body point per round as long as their work remains uninterrupted.|n")
-                                self.caller.db.body += 1
-                                self.caller.msg(f"|540Your new body value is:|n {self.caller.db.body}|n")
+                                caller.location.msg_contents(f"|230{self.caller} pulls bandages and ointments from their bag, and starts to mend their wounds.|n\n|540{caller} heals |n|0201|n |540body points.|n")
+                                caller.db.body += 1
+                                caller.msg(f"|540Your new body value is:|n {caller.db.body}|n")
                                 # Clean up
                                 # Set self.caller's combat_turn to 0. Can no longer use combat commands.
-                                loop.combatTurnOff(self.caller)
+                                loop.combatTurnOff(caller)
                                 loop.cleanup()
 
-                        elif self.target != self.caller:
-                            if (self.target.db.body + 1) > 3:
+                        elif target != caller:
+                            if (target.db.body + 1) > 3:
                                 # If so set body to 1
-                                self.target.db.body = 3
-                                self.caller.msg(f"|230{self.target.key} doesn't require the application of your chiurgical skills. They seem to be healthy enough.|n")
+                                target.db.body = 3
+                                caller.msg(f"|230{target.key} doesn't require the application of your chiurgical skills. They seem to be healthy enough.|n")
                                 return
                             else:
                                 # If not over 1, add points to total
-                                self.target.location.msg_contents(f"|230{self.caller.key} comes to {self.target.key}'s rescue, healing {self.target.key} for|n |0201|n |230body point.|n")
-                                self.target.db.body += 1
-                                self.target.msg(f"|540Your new body value is:|n {self.target.db.body}|n")
+                                target.location.msg_contents(f"|230{caller.key} comes to {target.key}'s rescue, healing {target.key} for|n |0201|n |230body point.|n")
+                                target.db.body += 1
+                                target.msg(f"|540Your new body value is:|n {target.db.body}|n")
                                 # Set self.caller's combat_turn to 0. Can no longer use combat commands.
                                 loop.combatTurnOff(self.caller)
                                 loop.cleanup()
 
-                    elif self.target.db.body <= 0:
-                        self.caller.location.msg_contents(f"|230{self.caller.key} comes to {self.target.key}'s rescue, though they are too fargone.\n{self.target.key} may require the aid of more sophisticated healing techniques.|n")
+                    elif target.db.body <= 0:
+                        caller.location.msg_contents(f"|230{caller.key} comes to {target.key}'s rescue, though they are too fargone.\n{target.key} may require the aid of more sophisticated healing techniques.|n")
                         return
                 else:
-                    self.caller.msg("|400You had better not try that.|n")
+                    caller.msg("|400You had better not try that.|n")
                     return
             else:
-                self.msg("You are too injured to act.")
+                caller.msg("You are too injured to act.")
                 return
         else:
-            self.msg("You need to wait until it is your turn before you are able to act.")
+            caller.msg("You need to wait until it is your turn before you are able to act.")
             return
