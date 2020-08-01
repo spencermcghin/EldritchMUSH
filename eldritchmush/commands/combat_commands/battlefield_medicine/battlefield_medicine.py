c360b72772f5ca21ff4dd32b93b88c5bddfac34a
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-01 08:49:58 -0700

    Updated BM command

--- eldritchmush/commands/combat_commands/battlefield_medicine/battlefield_medicine.py
+++ eldritchmush/commands/combat_commands/battlefield_medicine/battlefield_medicine.py
@@ -2,6 +2,7 @@
 from evennia import Command
 from world.combat_loop import CombatLoop
 from commands.combat import Helper
+from battlefield_med_handler import HealingHandler
 
 
 class CmdBattlefieldMedicine(Command):
@@ -24,47 +25,32 @@ class CmdBattlefieldMedicine(Command):
 
         # Get target if there is one
         target = self.caller.search(self.target)
+        caller = self.caller
 
         # Get target body and BM to validate target and caller has skill.
         target_body = target.db.body
-        battlefieldmedicine = self.caller.db.battlefieldmedicine
+        battlefieldmedicine = caller.db.battlefieldmedicine
 
-        if target in target.location.db.combat_loop:
+        if target.key in target.location.db.combat_loop:
             """
-            If target of skill is in combat, check to see if it's callers turn
-            """
+            If target of skill is in combat, use loop to resolve command and insert caller into loop.
+            Will need to wait until it is callers turn to act.
 
+            """
+            caller.msg(f"You try to heal {target.key}, but they are currently engaged in combat.")
+            loop = CombatLoop(caller, target)
+            loop.resolveCommand()
+
+            # Apply healing skills accordingly
+            healing_handler = HealingHandler(caller, target)
+            healing_handler.resolve_healing()
 
         else:
             if battlefieldmedicine and target_body is not None:
 
-                if 1 <= target_body <= 3:
-                    # Return message to area and caller
-                    if target == self.caller:
-                        # Check to see if caller would go over 1 body with application of skill.
-                        if (self.caller.db.body + 1) > 3:
-                            # If so set body to 1
-                            self.caller.db.body = 3
-                            self.caller.msg(f"|230{target.key} doesn't require the application of your chiurgical skills. They seem to be healthy enough.|n")
-                        else:
-                            # If not over 1, add points to total
-                            self.caller.location.msg_contents(f"|230{self.caller} pulls bandages and ointments from their bag, and starts to mend their wounds.|n\n|540{self.caller} heals |n|0201|n |540body point per round as long as their work remains uninterrupted.|n")
-                            self.caller.db.body += 1
-                            self.caller.msg(f"|540Your new body value is:|n {self.caller.db.body}|n")
-
-                    elif target != self.caller:
-                        if (target.db.body + 1) > 3:
-                            # If so set body to 1
-                            target.db.body = 3
-                            self.caller.msg(f"|230{target.key} doesn't require the application of your chiurgical skills. They seem to be healthy enough.|n")
-                        else:
-                            # If not over 1, add points to total
-                            target.location.msg_contents(f"|230{self.caller.key} comes to {target.key}'s rescue, healing {target.key} for|n |0201|n |230body point.|n")
-                            target.db.body += 1
-                            target.msg(f"|540Your new body value is:|n {target.db.body}|n")
-
-                elif target_body <= 0:
-                    self.caller.location.msg_contents(f"|230{self.caller.key} comes to {target.key}'s rescue, though they are too fargone.\n{target.key} may require the aid of more advanced chiurgical techniques.|n")
+                # Apply healing skills accordingly
+                healing_handler = HealingHandler(caller, target)
+                healing_handler.resolve_healing()
 
             else:
                 self.caller.msg("|400You had better not try that.|n")
