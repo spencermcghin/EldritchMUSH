a6140f069202023d5234d55f1b92f0b7bd08e9cc
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-02 08:53:29 -0700

    Added more stats to combat helper function

--- eldritchmush/commands/combat.py
+++ eldritchmush/commands/combat.py
@@ -279,6 +279,10 @@ class Helper():
         weakness = self.weaknessChecker(combatant.db.weakness)
         dmg_penalty = self.bodyChecker(combatant.db.body)
 
+        # Slot checks for sunder command
+        left_slot = combatant.db.left_slot
+        right_slot = combatant.db.right_slot
+
         melee_stats = {"melee": melee,
                        "bow": bow,
                        "bow_penalty": 2,
@@ -287,7 +291,9 @@ class Helper():
                        "weapon_level": weapon_level,
                        "wylding_hand": wylding_hand,
                        "weakness": weakness,
-                       "dmg_penalty": dmg_penalty}
+                       "dmg_penalty": dmg_penalty,
+                       "left_slot": left_slot,
+                       "right_slot": right_slot}
 
         return melee_stats
 
@@ -633,121 +639,6 @@ class CmdStagger(Command):
 
 
 
-# class CmdSunder(Command):
-#     """
-#     Issues a sunder command.
-#
-#     Usage:
-#
-#     sunder <target>
-#
-#     This will calculate an attack score based on your weapon and master of arms level.
-#     """
-#
-#     key = "sunder"
-#     help_category = "mush"
-#
-#     def parse(self):
-#         "Very trivial parser"
-#         self.target = self.args.strip()
-#
-#         if not self.target:
-#             self.caller.msg("|540Usage: sunder <target>|n")
-#             return
-#
-#     def func(self):
-#         h = Helper()
-#
-#         "Get level of master of arms for base die roll. Levels of gear give a flat bonus of +1/+2/+3."
-#         master_of_arms = self.caller.db.master_of_arms
-#         hasBow = self.caller.db.bow
-#         hasTwoHanded = self.caller.db.twohanded
-#         sundersRemaining = self.caller.db.sunder
-#         weapon_level = h.weaponValue(self.caller.db.weapon_level)
-#
-#         wylding_hand = self.caller.db.wyldinghand
-#
-#         target = self.caller.search(self.target)
-#
-#         # Get penalty to subtract from target
-#         target_penalty = self.caller.db.master_of_arms
-#
-#         if not target:
-#             self.caller.msg("|400There is nothing here that matches that description.|n")
-#             return
-#
-#         if target == self.caller:
-#             self.caller.msg(f"|400Don't sunder yourself {self.caller}!|n")
-#             return
-#
-#         if target.db.body is None:
-#             self.caller.msg("|400You had better not try that.|n")
-#             return
-#
-#         # Check for equip proper weapon type
-#         # Check for weakness on character
-#         weakness = h.weaknessChecker(self.caller.db.weakness)
-#         target_av = target.db.av
-#
-#         # Check for equip proper weapon type or weakness
-#         if weakness:
-#             self.caller.msg("|400You are too weak to use this attack.|n")
-#         elif hasBow:
-#             self.caller.msg("|540Before you can attack, you must first unequip your bow using the command setbow 0.")
-#         elif not hasTwoHanded:
-#             self.caller.msg("|540Before you can attack, you must first equip a two handed weapon using the command settwohanded 1.")
-#         else:
-#             if sundersRemaining > 0:
-#             # Return die roll based on level in master of arms or wylding hand.
-#                 if wylding_hand:
-#                     die_result = h.wyldingHand(wylding_hand)
-#                 else:
-#                     die_result = h.masterOfArms(master_of_arms)
-#
-#                 # Decrement amount of cleaves from amount in database
-#                 self.caller.db.sunder -= 1
-#
-#                 # Get final attack result and damage
-#                 weakness = h.weaknessChecker(self.caller.db.weakness)
-#                 dmg_penalty = h.bodyChecker(self.caller.db.body)
-#                 attack_result = (die_result + weapon_level) - dmg_penalty - weakness
-#                 shot_location = h.shotFinder(target.db.targetArray)
-#
-#                 # Return attack result message
-#                 if attack_result >= (target_av - target_penalty):
-#                     # if target has any more armor points left go through the damage subtractor
-#                     if target_av:
-#                         self.caller.location.msg_contents(f"|015{self.caller.key} sunders|n (|020{attack_result}|n) |015at {target.key} and hits|n (|400{target_av}|n), |015dealing|n |5402|n |015damage!|n")
-#                         # subtract damage from corresponding target stage (shield_value, armor, tough, body)
-#                         new_av = h.damageSubtractor(2, target)
-#                         # Update target av to new av score per damageSubtractor
-#                         target.db.av = new_av
-#                         target.msg(f"|540Your new total Armor Value is {new_av}:\nShield: {target.db.shield}\nArmor Specialist: {target.db.armor_specialist}\nArmor: {target.db.armor}\nTough: {target.db.tough}|n")
-#                     else:
-#                         # No target armor so subtract from their body total and hit a limb. Add logic from handler above. Leave in body handler in combat handler.
-#                         self.caller.location.msg_contents(f"|015{self.caller.key} strikes deftly|n (|020{attack_result}|n) |015at {target.key} and hits |n(|400{target_av}|n)|015, injuring their {shot_location} and dealing|n |5402|n |015damage!|n.")
-#                         if shot_location == "torso":
-#                             if target.db.body > 0:
-#                                 target.db.body = 0
-#                                 self.caller.location.msg_contents(f"|015{target.key} has been fatally wounded and is now bleeding to death. They will soon be unconscious.|n")
-#                             else:
-#                                 target.db.body -= 2
-#                                 target.msg(f"|540Your new body value is {target.db.body}|n")
-#                         else:
-#                             target.db.body -= 2
-#                             target.msg(f"|400You {shot_location} is now injured and have taken |n|5402|n|400 points of damage.|n")
-#                             # Send a message to the target, letting them know their body values
-#                             target.msg(f"|540Your new body value is {target.db.body}|n")
-#                             if -3 <= target.db.body <= 0:
-#                                 target.msg("|540You are bleeding profusely from many wounds and can no longer use any active martial skills.\nYou may only use the limbs that have not been injured.|n")
-#                                 target.location.msg_contents(f"|015{target.key} is bleeding profusely from many wounds and will soon lose consciousness.|n")
-#                             elif target.db.body <= -4:
-#                                 target.msg("|400You are now unconscious and can no longer move of your own volition.|n")
-#                                 target.location.msg_contents(f"|015{target.key} does not seem to be moving.|n")
-#                 else:
-#                     self.caller.location.msg_contents(f"|015{self.caller.key} swings wildly|n |400{attack_result}|n|015, |015missing {target.key}|n")
-#             else:
-#                self.caller.msg("|400You have 0 sunders remaining or do not have the skill.|n")
 
 """
 Knight commands
