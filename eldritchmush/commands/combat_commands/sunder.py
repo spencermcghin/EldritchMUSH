63f34063f52dc10ef9bb22cc0203386b2b680ea4
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-02 18:41:20 -0700

    Sunder draft

--- eldritchmush/commands/combat_commands/sunder.py
+++ eldritchmush/commands/combat_commands/sunder.py
@@ -85,7 +85,7 @@ class CmdSunder(Command):
 
                                         # Check target left and right slots for items. Decrement material value from right and then left.
                                         # If no more items, subtract damage as normal.
-                                        if target_stats.get("right_slot", ''):
+                                        if target_stats.get("right_slot", None):
                                             # Get item and material value for right slot.
                                             right_item = self.caller.search(target.db.right_slot[0])
                                             right_mv = right_item.db.material_value
@@ -98,7 +98,7 @@ class CmdSunder(Command):
                                                 right_mv -= 1
                                                 self.caller.location.msg_contents(f"|015{self.caller.key} strikes|n (|020{attack_result}|n) |015with great ferocity and damages {target.key}'s {right_item.key}|n (|400{target.db.av}|n).")
 
-                                        elif target_stats.get("left_slot", ''):
+                                        elif target_stats.get("left_slot", None):
                                             # Get item and material value for right slot.
                                             left_item = self.caller.search(target.db.left_slot[0])
                                             left_mv = left_item.db.material_value
