4808803f126cf194d6106143a3b464c9565f2930
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-04 21:44:08 -0700

    imported medicine

--- eldritchmush/commands/default_cmdsets.py
+++ eldritchmush/commands/default_cmdsets.py
@@ -19,8 +19,7 @@ from evennia.commands.default import general, building
 from evennia import CmdSet
 from commands import command
 from commands import combat
-from commands.combat_commands import strike, disengage, shoot, cleave, battlefield_medicine, stabilize, sunder, disarm, stagger, stun
-
+from commands.combat_commands import strike, disengage, shoot, cleave, battlefield_medicine, stabilize, sunder, disarm, stagger, stun, medicine
 from commands import npc
 from commands import dice
 
