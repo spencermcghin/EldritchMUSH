c887787f0de41cba33a87f64035d478a96764465
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-05 21:50:36 -0700

    testing npc

--- eldritchmush/typeclasses/rooms.py
+++ eldritchmush/typeclasses/rooms.py
@@ -29,18 +29,18 @@ class Room(DefaultRoom):
     """
 
     def at_object_receive(self, obj, source_location):
-        obj.msg("Hi there stranger.")
-        # if utils.inherits_from(obj, 'Npc'): # An NPC has entered
-        #     pass
-        # else:
-        #     if utils.inherits_from(obj, 'Character'):
-        #         # A PC has entered, NPC is caught above.
-        #         # Cause the character to look around
-        #         obj.execute_cmd('look')
-        #         for item in self.contents:
-        #             if utils.inherits_from(item, 'Npc'):
-        #                 # An NPC is in the room
-        #                 item.at_char_entered(obj)
+        # obj.msg("Hi there stranger.")
+        if utils.inherits_from(obj, 'Npc'): # An NPC has entered
+            pass
+        else:
+            if utils.inherits_from(obj, 'Character'):
+                # A PC has entered, NPC is caught above.
+                # Cause the character to look around
+                obj.execute_cmd('look')
+                for item in self.contents:
+                    if utils.inherits_from(item, 'Npc'):
+                        # An NPC is in the room
+                        item.at_char_entered(obj)
 
 
     def at_object_creation(self):
