6fbe71e638eaffec8eb61e0ece268f94f67e8e34
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-05 21:53:47 -0700

    Update characters

--- eldritchmush/typeclasses/characters.py
+++ eldritchmush/typeclasses/characters.py
@@ -108,10 +108,10 @@ class Character(DefaultCharacter):
             return text
 
     # Changed for AI room response
-    # def at_after_move(self, source_location):
-    #     """
-    #     Default is to look around after a move
-    #     Note:  This has been moved to room.at_object_receive
-    #     """
-    #     #self.execute_cmd('look')
-    #     pass
+    def at_after_move(self, source_location):
+        """
+        Default is to look around after a move
+        Note:  This has been moved to room.at_object_receive
+        """
+        #self.execute_cmd('look')
+        pass
