9796314e6007e2cd290313d0c3b6d3f690bfd846
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-07 15:10:11 -0700

    Added dict object to pull actives only

--- eldritchmush/typeclasses/npc.py
+++ eldritchmush/typeclasses/npc.py
@@ -1,4 +1,5 @@
 from typeclasses.characters import Character
+from commands.combat_commands import combat
 
 class Npc(Character):
     """
@@ -30,8 +31,15 @@ class MeleeSoldier(Npc):
     def command_picker(self, target):
         """
         When it is this npcs turn to act they will pick from their available combat commands at random.
+        1. Create dict of command key and self.db.<active_martial_skill_att> -> "stun": combat_stats.get(self.db.stun, 0).
+        Need to import Helper to make this work.
+        2. Every time the at_char_entered command fires, this is what happens:
+        3. Generate array of possible commands as ["strike", other values are made by calling the combat_bank keys based on value != 0]
+        4. Choose a random element of the array and execute_cmd
         """
-        # Form execute_cmd template, choosing from random commands.
+        # Form execute_cmd template, choosing from random commands
+
+        helper = Helper()
 
         action_string = f"strike {target}"
 
