a78efea88f9d1337dcfe04f151a09b92df3ddfd9
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-08 09:58:35 -0700

    debugging combat res for npcs

--- eldritchmush/world/combat_loop.py
+++ eldritchmush/world/combat_loop.py
@@ -243,8 +243,8 @@ class CombatLoop:
                     nextCharacter.execute_cmd("disengage")
 
         else:
-            self.caller.location.msg_contents("caught > 1 player in cleanup else")
-            if self.caller in self.combat_loop:
+            self.caller.location.msg_contents("caught < 1 player in cleanup else")
+            if self.caller in self.location.db.combat_loop:
                 self.caller.location.msg_contents(f"{self.caller} is still in the combat loop")
                 self.caller.location.msg_contents(f"Combat is now over for {loop.current_room}.")
                 self.removeFromLoop(self.caller)
