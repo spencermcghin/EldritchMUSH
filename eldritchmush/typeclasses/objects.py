cb99ffc4b1b79df912e3f8c588cdf29aa8f1dd3b
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-09 10:44:30 -0700

    added all blacksmith components

--- eldritchmush/typeclasses/objects.py
+++ eldritchmush/typeclasses/objects.py
@@ -284,4 +284,4 @@ class Forge(DefaultObject):
         self.db.desc = "\n|540This is a large forge as is used by a blacksmith in their trade. Metal items are heated here, until they are pliable enough to be molded and shaped by a mighty hammer and the smith's labor.|n\n|400OOG - Usage: \nforge <item>\nrepair <item>|n"
 
         # Add command set for interacting with box
-        self.cmdset.add_default(ForgeCmdSet, permanent=True)
+        self.cmdset.add_default(BlacksmithCmdSet, permanent=True)
