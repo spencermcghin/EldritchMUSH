22f37a655a202d9390dfbfc4aca2a166c4f47cfa
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-09 16:12:36 -0700

    testing blacksmith forge items

--- eldritchmush/commands/blacksmith.py
+++ eldritchmush/commands/blacksmith.py
@@ -20,7 +20,7 @@ class CmdForge(Command):
 
     def parse(self):
         "Very trivial parser"
-        self.item = self.args.strip()
+        self.item = self.args
 
 
     def func(self):
@@ -40,44 +40,46 @@ class CmdForge(Command):
         blacksmith_item = spawn({f"key": "{self.item}",
                                   "location": self.caller.location})[0]
 
-        if blacksmith_item:
-            # Check for items in callers inventory.
-            character_resources = {
-            "iron_ingots": self.caller.db.iron_ingots,
-            "cloth": self.caller.db.cloth,
-            "refined_wood": self.caller.db.refined_wood,
-            "leather": self.caller.db.leather
-            }
+        self.msg(f"{blacksmith_item}")
 
-            # Get item requirements
-            item_requirements = {
-            "iron_ingots": blacksmith_item.db.iron_ingots,
-            "cloth": blacksmith_item.db.cloth,
-            "refined_wood": blacksmith_item.db.refined_wood,
-            "leather": blacksmith_item.db.leather
-            }
-
-            requirements_checker = [
-            character_resources["iron_ingots"] >= item_requirements["iron_ingots"],
-            character_resources["cloth"] >= item_requirements["cloth"],
-            character_resources["refined_wood"] >= item_requirements["refined_wood"],
-            character_resources["leather"] >= item_requirements["leather"]
-            ]
-
-            # Check that all conditions in above list are true.
-            if all(requirements_checker):
-                self.msg(f"You forge a {self.item}")
-                # Get required resources and decrement from player totals.
-                self.caller.db.iron_ingots -= item_requirements["iron_ingots"]
-                self.caller.db.cloth -= item_requirements["cloth"]
-                self.caller.db.refined_wood -= item_requirements["refined_wood"]
-                self.caller.db.leather -= item_requirements["leather"]
-
-                # Give to blacksmith
-                blacksmith_item.move_to(self.caller, quiet=True)
-
-            else:
-                self.msg(f"You don't have the required resources for a {self.item}")
-                self.delete(blacksmith_item)
+        # if blacksmith_item:
+        #     # Check for items in callers inventory.
+        #     character_resources = {
+        #     "iron_ingots": self.caller.db.iron_ingots,
+        #     "cloth": self.caller.db.cloth,
+        #     "refined_wood": self.caller.db.refined_wood,
+        #     "leather": self.caller.db.leather
+        #     }
+        #
+        #     # Get item requirements
+        #     item_requirements = {
+        #     "iron_ingots": blacksmith_item.db.iron_ingots,
+        #     "cloth": blacksmith_item.db.cloth,
+        #     "refined_wood": blacksmith_item.db.refined_wood,
+        #     "leather": blacksmith_item.db.leather
+        #     }
+        #
+        #     requirements_checker = [
+        #     character_resources["iron_ingots"] >= item_requirements["iron_ingots"],
+        #     character_resources["cloth"] >= item_requirements["cloth"],
+        #     character_resources["refined_wood"] >= item_requirements["refined_wood"],
+        #     character_resources["leather"] >= item_requirements["leather"]
+        #     ]
+        #
+        #     # Check that all conditions in above list are true.
+        #     if all(requirements_checker):
+        #         self.msg(f"You forge a {self.item}")
+        #         # Get required resources and decrement from player totals.
+        #         self.caller.db.iron_ingots -= item_requirements["iron_ingots"]
+        #         self.caller.db.cloth -= item_requirements["cloth"]
+        #         self.caller.db.refined_wood -= item_requirements["refined_wood"]
+        #         self.caller.db.leather -= item_requirements["leather"]
+        #
+        #         # Give to blacksmith
+        #         blacksmith_item.move_to(self.caller, quiet=True)
+        #
+        #     else:
+        #         self.msg(f"You don't have the required resources for a {self.item}")
+        #         self.delete(blacksmith_item)
         else:
             self.msg("Foo")
