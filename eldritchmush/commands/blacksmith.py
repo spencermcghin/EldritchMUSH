88e28e3d05c54e668a6a9f2748538c85a43a3f52
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-09 15:53:05 -0700

    testing creating items

--- eldritchmush/commands/blacksmith.py
+++ eldritchmush/commands/blacksmith.py
@@ -37,28 +37,32 @@ class CmdForge(Command):
             self.msg("|400You are not trained in how to properly utilze a forge. Please find a blacksmith.|n")
             return
 
+
+        # Spawn item and move to callers inventory
+        blacksmith_item = spawn({f"key": "{self.item}",
+                                  "location": self.caller.location})
+
+
         # Check for items in callers inventory.
         item_requirements = [
-        self.caller.db.iron_ingots >= self.item.db.iron_ingots,
-        self.caller.db.cloth >= self.item.db.cloth,
-        self.caller.db.refined_wood >= self.item.db.refined_wood,
-        self.caller.db.leather >= self.item.db.leather
+        self.caller.db.iron_ingots >= blacksmith_item.db.iron_ingots,
+        self.caller.db.cloth >= blacksmith_item.db.cloth,
+        self.caller.db.refined_wood >= blacksmith_item.db.refined_wood,
+        self.caller.db.leather >= blacksmith_item.db.leather
         ]
 
         # Check that all conditions in above list are true.
         if all(item_requirements):
             self.msg(f"You forge a {self.item}")
             # Get required resources and decrement from player totals.
-            self.caller.db.iron_ingots -= self.item.db.iron_ingots
-            self.caller.db.cloth -= self.item.db.cloth
-            self.caller.db.refined_wood -= self.item.db.refined_wood
-            self.caller.db.leather -= self.item.db.leather
+            self.caller.db.iron_ingots -= blacksmith_item.db.iron_ingots
+            self.caller.db.cloth -= blacksmith_item.db.cloth
+            self.caller.db.refined_wood -= blacksmith_item.db.refined_wood
+            self.caller.db.leather -= blacksmith_item.db.leather
 
-            # Spawn item and move to callers inventory
-            blacksmith_item = spawn({f"key": "{self.item}",
-                                      "location": self.caller.location})
-
+            # Give to blacksmith
             blacksmith_item.move_to(self.caller, quiet=True)
 
         else:
             self.msg(f"You don't have the required resources for a {self.item}")
+            self.delete(blacksmith_item)
