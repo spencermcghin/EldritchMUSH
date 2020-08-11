d6a2e6f2d9eaaa17e4d005a3a161a1e48dbc2982
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-10 21:45:19 -0700

    testing parser

--- eldritchmush/commands/command.py
+++ eldritchmush/commands/command.py
@@ -220,11 +220,11 @@ class CmdGive(Command):
 
         args_list = args.split("/")
         item = args_list[0]
-        qty, target = re.split("=| to ", args_list[1].strip())
+        qty, target = re.split("=| to ", args_list[1])
 
         self.item = item
         self.qty = qty
-        self.target = target
+        self.target = target.strip()
 
     def func(self):
 
