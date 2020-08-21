92d4a1f113ec167a22d16f897bc74f008d57844e
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-21 15:20:59 -0700

    testing search for patch kit

--- eldritchmush/commands/command.py
+++ eldritchmush/commands/command.py
@@ -2414,7 +2414,7 @@ class CmdPatch(Command):
                                       nofound_string=f"|300{self.item} not found.|n",
                                       multimatch_string="|430Your search has more than one result. Please be more specific.|n")
 
-        patch_kit = self.caller.search("patch kit",
+        patch_kit = self.caller.search("1-patch kit",
                                        location=self.caller,
                                        nofound_string="You are not carrying any patch kits.",
                                        multimatch_string="|430Your search has more than one result. Please be more specific.|n")
