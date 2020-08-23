b61a14a313b30b10944c17dc2b9757f7b5c45125
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-23 15:03:48 -0700

    added new give to commamnds

--- eldritchmush/commands/default_cmdsets.py
+++ eldritchmush/commands/default_cmdsets.py
@@ -109,6 +109,7 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         self.add(command.SetTwoHanded())
         self.add(command.SetBow())
         self.add(command.CmdGive())
+        self.add(command.CmdGet())
         self.add(command.SetMelee())
         self.add(command.SetWeaponValue())
         self.add(command.CmdSmile())
