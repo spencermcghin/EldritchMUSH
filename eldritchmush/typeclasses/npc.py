5d47e95d73673a6171b3ae535f72f9e0a0c0320f
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-28 08:56:12 -0700

    Added archer NPCs

--- eldritchmush/typeclasses/npc.py
+++ eldritchmush/typeclasses/npc.py
@@ -327,6 +327,154 @@ class GreenMeleeSoldierTwoHanded(GreenMeleeSoldierOneHanded):
 
         return action_string
 
+class GreenMeleeSoldierBow(GreenMeleeSoldierOneHanded):
+    """
+    Generic solider NPC
+    """
+    def at_object_creation(self):
+        "This is called when object is first created, only."
+        # Entries for general stats
+        self.db.master_of_arms = 1
+        self.db.armor = 0
+        self.db.armor_specialist = 1
+        self.db.tough = 1
+        self.db.body = 3
+        self.db.av = 0
+        self.db.resilience = 1
+
+        # Entries for hit location system
+        self.db.targetArray = ["torso", "torso", "right arm", "left arm", "right leg", "left leg"]
+        self.db.right_arm = 1
+        self.db.left_arm = 1
+        self.db.right_leg = 1
+        self.db.left_leg = 1
+        self.db.torso = 1
+
+        # Entries for status effects
+        self.db.weakness = 0
+        self.db.bleed_points = 3
+        self.db.death_points = 3
+
+        # Entries for combat
+        self.db.resist = 0
+        self.db.disarm = 0
+        self.db.cleave = 0
+        self.db.sunder = 0
+        self.db.stun = 0
+        self.db.stagger = 0
+        self.db.weapon_level = 0
+        self.db.shield_value = 0
+        self.db.wyldinghand = 0
+        self.db.shield = 0
+        self.db.bow = 0
+        self.db.activemartialskill = 1
+        self.db.combat_turn = 1
+        self.db.in_combat = 0
+        self.db.left_slot = []
+        self.db.right_slot = []
+        self.db.body_slot = []
+        self.db.is_aggressive = False
+        self.db.skip_turn = False
+
+        # Entries for following
+        self.db.isLeading = False
+        self.db.leader = []
+        self.db.isFollowing = False
+        self.db.followers = []
+
+        # Entries for economy
+        self.db.iron_ingots = 0
+        self.db.refined_wood = 0
+        self.db.leather = 0
+        self.db.cloth = 0
+        self.db.gold = 0
+        self.db.silver = 0
+        self.db.copper = 0
+        self.db.arrows = 0
+
+    def make_equipment(self):
+        prototype = prototypes.search_prototype("bow", require_single=True)
+        armor_prototype = prototypes.search_prototype("iron_coat_of_plates", require_single=True)
+        # Get prototype data
+        bow_data = prototype[0]
+        armor_data = armor_prototype[0]
+        # Spawn item using data
+        weapon_item = spawn(bow_data)
+        armor_item = spawn(armor_data)
+        # Move item to caller's inventory
+        weapon_item[0].move_to(self, quiet=True)
+        armor_item[0].move_to(self, quiet=True)
+        # Equip items
+        self.execute_cmd('equip bow')
+        self.execute_cmd('equip hardened iron coat of plates')
+
+
+    def at_char_entered(self, character):
+        # Do stuff to equip your character
+        # Choose a random command and run it
+        if self.db.is_aggressive and self.db.bleed_points:
+            inventory = self.contents
+            weapons = [item for item in inventory if item.db.damage or item.db.is_bow]
+
+            if len(weapons) == 0:
+                self.make_equipment()
+
+            command = self.command_picker(character)
+            self.execute_cmd(command)
+        else:
+            return
+
+
+    def command_picker(self, target):
+        """
+        When it is this npcs turn to act they will pick from their available combat commands at random.
+        1. Create dict of command key and self.db.<active_martial_skill_att> -> "stun": combat_stats.get(self.db.stun, 0).
+        Need to import Helper to make this work.
+        2. Every time the at_char_entered command fires, this is what happens:
+        3. Generate array of possible commands as ["strike", other values are made by calling the combat_bank keys based on value != 0]
+        4. Choose a random element of the array and execute_cmd
+        """
+        # Form execute_cmd template, choosing from random commands
+        # helper = Helper(self)
+        # combat_bank = helper.activeMartialCounter(self)
+        amSkills = {
+        "stun": self.db.stun,
+        "disarm": self.db.disarm,
+        "sunder": self.db.sunder,
+        "stagger": self.db.stagger,
+        "cleave": self.db.cleave
+        }
+
+        # Generate an array of possible commands.
+        ams_commands = [(command,)*value for command, value in amSkills.items() if value != 0]
+        flat_ams_commands = [attack for groups in ams_commands for attack in groups]
+        # Add free command to list
+        flat_ams_commands.append("shoot")
+        # Choose random command
+        chosen_command = random.choice(flat_ams_commands)
+        # Catch exceptions to running active martial skills - weakness condition
+        # Make sure npc is equipped:
+
+        if not self.db.right_slot or self.db.left_slot:
+            self.execute_cmd('equip bow')
+            pass
+
+        # Random command is strike. Run it, else check to make sure npc can run an active martial skill w/o exception.
+        if chosen_command not in amSkills:
+            if not target.db.bleed_points:
+                action_string = 'disengage'
+            else:
+                action_string = chosen_command + ' ' + target.key
+        else:
+            # If target is in dying count, disengage, else run free combat command.
+            if not target.db.bleed_points:
+                action_string = 'disengage'
+            else:
+                chosen_command = 'shoot' if self.db.weakness else chosen_command
+                # Establish command string
+                action_string = chosen_command + ' ' + target.key
+
+        return action_string
 
 
 class BlueMeleeSoldierOneHanded(Npc):
