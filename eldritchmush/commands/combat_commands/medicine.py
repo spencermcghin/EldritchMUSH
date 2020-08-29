d1b5be8bd60d778e7c36cf008f1ee941f15dcdf1
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-29 11:59:43 -0700

    updating medicine

--- eldritchmush/commands/combat_commands/medicine.py
+++ eldritchmush/commands/combat_commands/medicine.py
@@ -60,17 +60,20 @@ class CmdMedicine(Command):
                     new_bp_value = target_total_bleed_points + target_resilience + medicine
                     if (target.db.bleed_points == target_total_bleed_points) and target_body == 0:
                         target.db.body += 1
-                        caller.location.msg_contents(f"|025{caller.key} performs some minor healing techniques and provides|n (|430{medicine}|n) |025points of aid to {target.key}.|n")
+                        caller.location.msg_contents(f"|025{caller.key} performs some minor healing techniques and provides|n (|4301|n) |025points of aid to {target.key}.|n")
 
                     elif (target.db.bleed_points < target_total_bleed_points):
-                        caller.location.msg_contents(f"|025{caller.key} performs some minor healing techniques and provides|n (|430{medicine}|n) |025points of aid to {target.key}.|n")
                         if new_bp_value > target_total_bleed_points:
                             # Set to max bleed_points
                             target.db.bleed_points = target_total_bleed_points
                             # Add extra to body
                             excess_bp = new_bp_value - target_total_bleed_points
                             if excess_bp + target_body > 1:
+                                # current body = 0
+                                # bleed points = 2
+                                # heal for 3
                                 target.db.body = 1
+                                caller.location.msg_contents(f"|025{caller.key} performs some minor healing techniques and provides|n (|430{medicine}|n) |025points of aid to {target.key} (up to 1 body).|n")
                             else:
                                 target.db.body += excess_bp
                         else:
