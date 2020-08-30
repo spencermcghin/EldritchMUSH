22586bfd2c4e1e69e9993c9a54bfdc8d3cc1d590
Spencer McGhin <spencer.mcghin@gmail.com>
2020-08-30 08:10:19 -0700

    removed default material value

--- eldritchmush/typeclasses/objects.py
+++ eldritchmush/typeclasses/objects.py
@@ -344,7 +344,6 @@ class BlacksmithObject(DefaultObject):
 class WeaponObject(BlacksmithObject):
     def at_object_creation(self):
         self.db.damage = 0
-        self.db.material_value = 0
         self.db.broken = False
         self.db.twohanded = False
         self.db.trait_one = []
