e5853ae18e576ca3c93a288b9f4c27b887c6fa38
Spencer McGhin <spencer.mcghin@gmail.com>
2020-09-03 14:53:35 -0700

    updating resist

--- eldritchmush/commands/combat_commands/resist.py
+++ eldritchmush/commands/combat_commands/resist.py
@@ -18,23 +18,15 @@ class CmdResist(Command):
     help_category = "mush"
 
     def func(self):
-        h = Helper()
+        h = Helper(self.caller)
 
-        "Get level of master of arms for base die roll. Levels of gear give a flat bonus of +1/+2/+3."
         resistsRemaining = self.caller.db.resist
-        master_of_arms = self.caller.db.master_of_arms
-        wylding_hand = self.caller.db.wyldinghand
-        weapon_level = h.weaponValue(self.caller.db.weapon_level)
         caller = self.caller
+        weakness = h.weaknessChecker(caller.db.weakness)
 
-        # Check for weakness on character
-        weakness = h.weaknessChecker(self.caller.db.weakness)
-
-        if target:
-            loop = CombatLoop(self.caller, target)
-            loop.resolveCommand()
-        else:
-            return
+        # Reconcile against combat loop to get turn.
+        loop = CombatLoop(caller, target=None)
+        loop.resolveCommand()
 
         if caller.db.combat_turn:
             if h.canFight(caller):
@@ -44,27 +36,29 @@ class CmdResist(Command):
                         self.caller.msg(f"|430You are at maximum tough and body points.|n")
                         return
 
-                    if caller.db.tough < caller.db.total_tough and target.db.body == 3:
+                    if (caller.db.tough < caller.db.total_tough) and caller.db.body == 3:
                         caller.db.tough +=1
-                        
+                        result = "1 tough"
 
-                    elif target.db.body < 3:
+                    elif caller.db.body < 3:
                         caller.db.body += 1
+                        result = "1 body"
 
-                    caller.location.msg_contents(f"{caller.key} resist the brunt of the attack.")
-                    caller.msg("You recover")
+                    else:
+
+
+                    caller.location.msg_contents(f"{caller.key} resists the brunt of the attack.")
+                    caller.msg(f"You recover {result}.")
+
+                    # End turn and clean up
+                    loop.combatTurnOff(caller)
+                    loop.cleanup()
 
                 else:
                     self.msg("|400You have 0 resists remaining or do not have the skill.\nPlease choose another action.|n")
+                    return
             else:
                 caller.msg("|400You are too injured to act.|n")
-                return
-
-            # Clean up in combat loop
-            if (caller in caller.location.db.combat_loop) or (target in caller.location.db.combat_loop):
-                loop.combatTurnOff(caller)
-                loop.cleanup()
-            else:
                 return
         else:
             caller.msg("|430You need to wait until it is your turn before you are able to act.|n")
