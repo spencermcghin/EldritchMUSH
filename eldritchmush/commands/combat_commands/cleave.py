885a7ad51a0f95c256ad20605d9621dacd0ed91a
nhillen <nhillen@gmail.com>
2020-09-05 16:48:48 -0700

    Added helper functions and renamed some variables

--- eldritchmush/commands/combat_commands/cleave.py
+++ eldritchmush/commands/combat_commands/cleave.py
@@ -18,17 +18,20 @@ class CmdCleave(Command):
     key = "cleave"
     help_category = "mush"
 
+    def __init__(self):
+        self.target = ''
+
     def parse(self):
-        "Very trivial parser"
+        # Very trivial parser
         self.target = self.args.strip()
 
     def func(self):
         if not self.args:
             self.msg("|430Usage: cleave <target>|n")
             return
-		
-		combatant = self.caller
-		
+
+        combatant = self.caller
+
         # Init combat helper functions
         h = Helper(combatant)
 
@@ -47,43 +50,43 @@ class CmdCleave(Command):
             combat_stats = h.getMeleeCombatStats(combatant)
             right_hand_item = combat_stats.get("right_slot", None)
             left_hand_item = combat_stats.get("left_slot", None)
-            cleavesRemaining = combatant.db.cleave
+            cleaves_remaining = combatant.db.cleave
 
             if combat_stats.get("two_handed", False):
-                if cleavesRemaining > 0:
+                if cleaves_remaining > 0:
 
                     die_result = h.fayneChecker(combat_stats.get("master_of_arms", 0), combat_stats.get("wylding_hand", 0))
 
                     # Get damage result and damage for weapon type
-                    attack_result = (die_result + combatant.db.weapon_level) - combat_stats.get("dmg_penalty", 0) - combat_stats.get("weakness", 0)
+                    attack_result = (die_result + combatant.db.weapon_level) - h.dmgPenalty() - h.weaknessPenalty()
                     damage = 2 if combat_stats.get("two_handed", False) else 1
                     target_av = target.db.av
                     shot_location = h.shotFinder(target.db.targetArray)
 
-                    if h.canFight(combatantr):
+                    if h.canFight(combatant):
                         if h.isAlive(target):
-                            if not combat_stats.get("weakness", 0):
+                            if not h.weaknessPenalty():
                                     if attack_result >= target_av:
-									
-										combatMessage = "|025{0} strikes|n (|020{1}|n) |025with great ferocity and cleaves {2}'s {3}|n (|400{4}|n)|025, dealing|n (|430{5}|n) |025damage|n".format(combatant.key, attack_result, target.key, shot_location, target_av, damage)
+
+                                        combat_message = "|025{0} strikes|n (|020{1}|n) |025with great ferocity and cleaves {2}'s {3}|n (|400{4}|n)|025, dealing|n (|430{5}|n) |025damage|n".format(combatant.key, attack_result, target.key, shot_location, target_av, damage)
 
                                         # Decrement amount of cleaves from amount in database
-                                        cleavesRemaining -= 1
+                                        cleaves_remaining -= 1
                                         if shot_location == "torso" and target.db.body > 0:
                                             target.db.body = 0
-											self.msg(f"|430{target.key} has been fatally wounded and is now bleeding to death. They will soon be unconscious.|n")
+                                            self.msg(f"|430{target.key} has been fatally wounded and is now bleeding to death. They will soon be unconscious.|n")
                                         else:
                                             h.deathSubtractor(damage, target, combatant)
                                     else:
-										combatMessage = "|025{0} swings ferociously|n (|030{1}|n) |025at {2}|n (|400{3}|n)|025, but misses.|n)".format(combatant.key,attack_result,target.key,target_av )
+                                        combat_message = "|025{0} swings ferociously|n (|030{1}|n) |025at {2}|n (|400{3}|n)|025, but misses.|n)".format(combatant.key,attack_result,target.key,target_av )
                                     
-									# Clean up
+                                    # Clean up
                                     # Set combatant's combat_turn to 0. Can no longer use combat commands.
-									loop.verboseEndTurn(combatant,combatMessage)
+                                    loop.verboseEndTurn(combatant,combat_message)
                             else:
                                 self.msg("|400You are too weak to use this attack.|n")
                         else:
-                            self.msg(f"|430{target.key} is dead. You only further mutiliate their body.|n")
+                            self.msg(f"|430{target.key} is dead. You only further mutilate their body.|n")
                             combatant.location.msg_contents(f"|025{combatant.key} further mutilates the corpse of {target.key}.|n")
                     else:
                         self.msg("|400You are too injured to act.|n")
