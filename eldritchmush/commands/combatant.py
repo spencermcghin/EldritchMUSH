b7ae88cfd798549421e920de65d51e503e4bafbc
nhillen <nhillen@gmail.com>
2020-09-10 01:40:46 -0700

    Sexy New Stun!  Checked in and Working!

--- eldritchmush/commands/combatant.py
+++ eldritchmush/commands/combatant.py
@@ -11,6 +11,7 @@ class Combatant:
         self.caller = caller
         self.helper = Helper(caller)
         self.name = self.caller.key
+        self.combatStats = self.helper.getMeleeCombatStats(self.caller);
 
     def canFight(self):
         return self.helper.canFight(self.caller)
@@ -146,3 +147,78 @@ class Combatant:
 
     def removeWeakness(self):
         self.caller.db.weakness = 0
+
+    #Now Combat
+    def stunsRemaining(self):
+        return self.caller.db.stun
+
+    def hasStunsRemaining(self,message=None):
+        if message and self.stunsRemaining() <= 0:
+            self.message(message)
+
+        return self.stunsRemaining()
+
+    def getRightHand(self):
+        return self.combatStats.get("right_slot", '')
+
+    def getLeftHand(self):
+        return self.combatStats.get("left_slot", '')
+
+    def isArmed(self,message=None):
+        if message and not self.getRightHand() and not self.getLeftHand():
+            self.message(message)
+            return False
+        else:
+            return True
+
+    def isUnarmed(self,message=None):
+        if message and self.isArmed():
+            self.message(message)
+            return False
+        else:
+            return True
+
+    def rollAttack(self):
+        die_result = self.helper.fayneChecker(self.combatStats.get("master_of_arms", 0), self.combatStats.get("wylding_hand", 0))
+
+        # Get damage result and damage for weapon type
+        attack_result = (die_result + self.caller.db.weapon_level) - self.combatStats.get("dmg_penalty",
+                                                                                      0) - self.combatStats.get("weakness",
+                                                                                                            0) - self.combatStats.get(
+            "stun_penalty", 0)
+        return attack_result
+
+    def isTwoHanded(self):
+        return self.combatStats.get("two_handed", 0)
+
+    def av(self):
+        return self.caller.db.av
+
+    def determineHitLocation(self,victim):
+        return self.helper.shotFinder(self.caller.db.targetArray)
+
+    def isAlive(self,message=None):
+        is_alive = True if self.caller.db.death_points else False
+        if message and not is_alive:
+            self.message(message)
+
+        return is_alive
+
+    def hasWeakness(self,message=None):
+        weakness = self.combatStats.get("weakness", 0)
+        if message and weakness:
+            self.message(message)
+        return weakness
+
+    def setStuns(self, value):
+        self.caller.db.stun = value
+
+    def decreaseStuns(self, value):
+        new_stun_value = self.stunsRemaining() - value
+        if new_stun_value < 0 or value < 0:
+            new_stun_value = 0
+
+        self.setStuns(new_stun_value)
+
+    def stun(self):
+        self.caller.db.skip_turn = True
