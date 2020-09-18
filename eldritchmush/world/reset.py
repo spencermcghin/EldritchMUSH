a98f40399f25849b245379cc1445a05c799e35ba
Spencer McGhin <spencer.mcghin@gmail.com>
2020-09-17 21:35:26 -0700

    testing reseter

--- eldritchmush/world/reset.py
+++ eldritchmush/world/reset.py
@@ -3,9 +3,9 @@ from typeclasses.characters import Character
 def reset_runner():
     # Get all characters
     characters = [character for character in Character.objects.all()]
-
-    for character in characters:
-        character.reset_stats()
+    print(characters)
+    # for character in characters:
+    #     character.reset_stats()
 
 if __name__ == "__main__":
     reset_runner()
