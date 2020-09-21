537c9a6bc720b14b4506e49eaaccc1d3de5f4f97
nhillen <nhillen@gmail.com>
2020-09-20 20:42:45 -0700

    Check shield stuff

--- eldritchmush/commands/combatant.py
+++ eldritchmush/commands/combatant.py
@@ -247,9 +247,9 @@ class Combatant:
     def getShield(self):
         right_hand = self.getRightHand()
         left_hand = self.getLeftHand()
-        if right_hand.db.is_shield and right_hand.db.broken == False:
+        if right_hand and right_hand.db.is_shield and right_hand.db.broken == False:
             return right_hand, "right arm"
-        elif left_hand.db.is_shield and left_hand.db.broken == False:
+        elif left_hand and left_hand.db.is_shield and left_hand.db.broken == False:
             return left_hand, "left arm"
         else:
             return None, None
