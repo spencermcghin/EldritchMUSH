676db9cf1b2c658dc8a6ba4bc1952f69aa26432c
Spencer McGhin <spencer.mcghin@gmail.com>
2020-09-26 11:36:24 -0700

    updated strike av report logic

--- eldritchmush/commands/combat_commands/strike.py
+++ eldritchmush/commands/combat_commands/strike.py
@@ -62,18 +62,18 @@ class CmdStrike(Command):
                     if attack_result >= victim.av:
                         if not victim.blocksWithShield(shot_location):
                             # Get damage result and damage for weapon type
+                            combatant.broadcast(
+                            f"{combatant.name} |025strikes deftly|n (|020{attack_result}|n) |025at|n {victim.name} |025and hits|n (|400{victim.av}|n), |025dealing|n (|430{combatant.getDamage()}|n) |025damage!|n")
                             victim.takeDamage(combatant, combatant.getDamage(), shot_location)
                             victim.reportAv()
-                            combatant.broadcast(
-                                f"|025{combatant.name} strikes deftly|n (|020{attack_result}|n) |025at {victim.name} and hits|n (|400{victim.av}|n), |025dealing|n (|430{combatant.getDamage()}|n) |025damage!|n")
                         else:
                             combatant.broadcast(
-                                f"|025{combatant.name} strikes deftly|n (|020{attack_result}|n) |025at {victim.name} and hits|n (|400{victim.av}|n), |025but {victim.name} blocks with their shield |n")
+                            f"|025{combatant.name} strikes deftly|n (|020{attack_result}|n) |025at {victim.name} and hits|n (|400{victim.av}|n), |025but|n {victim.name} |025blocks with their shield.|n")
                     else:
-                        combatant.broadcast(f"|025{combatant.name} swings wildly|n (|400{attack_result}|n)|025, missing {victim.name}|n (|020{victim.av}|n)")
+                        combatant.broadcast(f"|025{combatant.name} swings wildly|n (|400{attack_result}|n)|025, missing|n {victim.name} (|020{victim.av}|n)|025.|n")
                 else:
-                    combatant.message(f"|400{victim.name} is dead. You only further mutiliate their body.|n")
-                    combatant.broadcast(f"|025{combatant.name} further mutilates the corpse of {victim.name}.|n")
+                    combatant.message(f"{victim.name} |400is dead. You only further mutiliate their body.|n")
+                    combatant.broadcast(f"{combatant.name} |025further mutilates the corpse of {victim.name}.|n")
                 # Clean up
                 # Set self.caller's combat_turn to 0. Can no longer use combat commands.
                 loop.combatTurnOff(self.caller)
