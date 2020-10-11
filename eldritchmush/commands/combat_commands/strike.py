4990e3cb0a351dc0704f226a02be2fcf0b3be892
Spencer McGhin <spencer.mcghin@gmail.com>
2020-10-11 10:20:09 -0700

    alive checker updates. replaced sunder item checks

--- eldritchmush/commands/combat_commands/strike.py
+++ eldritchmush/commands/combat_commands/strike.py
@@ -61,26 +61,22 @@ class CmdStrike(Command):
 
         if combatant.hasTurn(f"|430You need to wait until it is your turn before you are able to act.|n"):
             if combatant.isArmed(f"|430Before you strike you must equip a melee weapon using the command equip <weapon name>.|n"):
-                if victim.isAlive:
-                    # Check if damage bonus comes from fayne or master_of_arms
-                    attack_result = combatant.rollAttack()
-                    shot_location = combatant.determineHitLocation(victim)
+                # Check if damage bonus comes from fayne or master_of_arms
+                attack_result = combatant.rollAttack()
+                shot_location = combatant.determineHitLocation(victim)
 
-                    if attack_result >= victim.av:
-                        if not victim.blocksWithShield(shot_location):
-                            # Get damage result and damage for weapon type
-                            combatant.broadcast(
-                            f"{combatant.name} |025strikes deftly|n (|020{attack_result}|n) |025at|n {victim.name} |025and hits|n (|400{victim.av}|n), |025dealing|n (|430{combatant.getDamage()}|n) |025damage!|n")
-                            victim.takeDamage(combatant, combatant.getDamage(), shot_location)
-                            victim.reportAv()
-                        else:
-                            combatant.broadcast(
-                            f"|025{combatant.name} strikes deftly|n (|020{attack_result}|n) |025at {victim.name} and hits|n (|400{victim.av}|n), |025but|n {victim.name} |025blocks with their shield.|n")
+                if attack_result >= victim.av:
+                    if not victim.blocksWithShield(shot_location):
+                        # Get damage result and damage for weapon type
+                        combatant.broadcast(
+                        f"{combatant.name} |025strikes deftly|n (|020{attack_result}|n) |025at|n {victim.name} |025and hits|n (|400{victim.av}|n), |025dealing|n (|430{combatant.getDamage()}|n) |025damage!|n")
+                        victim.takeDamage(combatant, combatant.getDamage(), shot_location)
+                        victim.reportAv()
                     else:
-                        combatant.broadcast(f"|025{combatant.name} swings wildly|n (|400{attack_result}|n)|025, missing|n {victim.name} (|020{victim.av}|n)|025.|n")
+                        combatant.broadcast(
+                        f"|025{combatant.name} strikes deftly|n (|020{attack_result}|n) |025at {victim.name} and hits|n (|400{victim.av}|n), |025but|n {victim.name} |025blocks with their shield.|n")
                 else:
-                    combatant.message(f"{victim.name} |400is dead. You only further mutiliate their body.|n")
-                    combatant.broadcast(f"{combatant.name} |025further mutilates the corpse of {victim.name}.|n")
+                    combatant.broadcast(f"|025{combatant.name} swings wildly|n (|400{attack_result}|n)|025, missing|n {victim.name} (|020{victim.av}|n)|025.|n")
                 # Clean up
                 # Set self.caller's combat_turn to 0. Can no longer use combat commands.
                 loop.combatTurnOff(self.caller)
