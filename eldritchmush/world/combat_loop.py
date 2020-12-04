0982a99b0f001eb0a89f76b0d99197bc8eaa54b0
Spencer McGhin <spencer.mcghin@gmail.com>
2020-12-03 19:49:17 -0800

    added bow check in npc combat loop

--- eldritchmush/world/combat_loop.py
+++ eldritchmush/world/combat_loop.py
@@ -269,11 +269,11 @@ class CombatLoop:
 							# Check to make sure carried item does damage
 							hasRightItem = True if nextCharacter.db.right_slot else False
 							rightItem = nextCharacter.db.right_slot[0] if hasRightItem else False
-							rightDamage = rightItem.db.damage if rightItem.db.damage else False
+							rightDamage = rightItem.db.damage if rightItem.db.damage or rightItem.db.is_bow else False
 
 							hasLeftItem = True if nextCharacter.db.left_slot else False
 							leftItem = nextCharacter.db.left_slot[0] if hasLeftItem else False
-							leftDamage = leftItem.db.damage if leftItem.db.damage else False
+							leftDamage = leftItem.db.damage if leftItem.db.damage or leftItem.db.is_bow else False
 
 							if (rightDamage or leftDamage):
 								random_target = random.choice(targets)
