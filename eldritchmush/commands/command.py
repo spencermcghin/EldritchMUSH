7031d913986f35f7ed17c12a1e8dc7daeb4edfde
Spencer McGhin <spencer.mcghin@gmail.com>
2020-12-05 21:12:02 -0800

    added import

--- eldritchmush/commands/command.py
+++ eldritchmush/commands/command.py
@@ -17,6 +17,7 @@ from evennia.prototypes import prototypes
 from evennia.commands.default.muxcommand import MuxCommand
 from evennia import default_cmds, utils, search_object, spawn
 from evennia.utils import evtable
+from typeclasses.objects import Container
 
 _SEARCH_AT_RESULT = utils.object_from_module(settings.SEARCH_AT_RESULT)
 
