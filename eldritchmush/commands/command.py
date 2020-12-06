19f1a0f82c1456a1013412ebb5cbddf9f32cfd96
Spencer McGhin <spencer.mcghin@gmail.com>
2020-12-05 20:49:08 -0800

    importing syntax

--- eldritchmush/commands/command.py
+++ eldritchmush/commands/command.py
@@ -17,7 +17,7 @@ from evennia.commands.default.muxcommand import MuxCommand
 from evennia import default_cmds, utils, search_object, spawn
 from evennia.utils import evtable
 from commands.fortunestrings import FORTUNE_STRINGS
-from typeclasses.objects import Container
+from typeclasses.objects import *
 
 _SEARCH_AT_RESULT = utils.object_from_module(settings.SEARCH_AT_RESULT)
 
