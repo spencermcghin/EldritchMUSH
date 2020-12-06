8233e3390deaf66f802a1a0dc9d696cb257d0352
Spencer McGhin <spencer.mcghin@gmail.com>
2020-12-05 21:02:54 -0800

    trying to fix import

--- eldritchmush/commands/command.py
+++ eldritchmush/commands/command.py
@@ -18,6 +18,7 @@ from evennia.commands.default.muxcommand import MuxCommand
 from evennia import default_cmds, utils, search_object, spawn
 from evennia.utils import evtable
 from typeclasses import objects
+from objects import Container
 
 _SEARCH_AT_RESULT = utils.object_from_module(settings.SEARCH_AT_RESULT)
 
